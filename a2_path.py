503337198833c3dc3f5d83207b5824d09f598f39
PunnyGamer <joshua.galvao123@gmail.com>
2025-10-29 12:01:56 +0000

    blep

--- a2_path.py
+++ a2_path.py
@@ -8,13 +8,45 @@ def safe(state):
     else:
         return False
     
-def tuptualstate(state):
+def tuptlestate(state):
     tup=[]
     for row in state.grid:
         tup.append(tuple(row))
     return tuple(tup)
     
 def path_BFS(start,end):
-    
-    
-    return 
+    if not safe(start) or not safe(end):
+        return None
+
+    start_t = tuptlestate(start)
+    end_t = tuptlestate(end)
+
+    queue = deque([(start, [start_t])])
+    visited = {start_t}
+
+    while queue:
+        current, path = queue.popleft()
+        if tuptlestate(current) == end_t:
+            return path
+
+        for next_state in current.moves():
+            if not safe(next_state):
+                continue
+            key = tuptlestate(next_state)
+            if key not in visited:
+                visited.add(key)
+                queue.append((next_state, path + [key]))
+    return None
+
+
+def path_DFS(start,end):
+    return
+
+def path_IDDFS(start,end):
+    return
+
+def path_astar(start,end):
+    return
+
+def compare(state):
+    return
