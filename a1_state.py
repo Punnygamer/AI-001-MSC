3ed8771853641d3996dd242575035499d75f8d11
PunnyGamer <joshua.galvao123@gmail.com>
2025-10-31 08:54:57 +0000

    state update

--- a1_state.py
+++ a1_state.py
@@ -34,14 +34,21 @@ class State():
         return string
     
     #checks to see if the coordinates is in bounds
-    def in_bounds(self, y, x):
+    def in_grid(self, y, x):
         if 0 <= y < self.rows and 0 <= x < self.cols:
             return True
         else:
             return False
-    
+        
+    #checks if a cell is active
+    def is_active(self, y, x):
+        if self.grid[y][x] > 0:
+            return True
+        else:
+            return False
+        
     #returns all cells ajacent to the coordinates of the current one 
-    def adjacent_cells(self, y, x):
+    def adjacent_cells(self,y,x):
         ajcells=[-1,0,1]
         for ajy in ajcells:
             for ajx in ajcells:
@@ -49,13 +56,10 @@ class State():
                     continue
                 nx= x +ajx
                 ny=  y+ ajy
-                if self.in_bounds(ny, nx):
-                    yield (ny, nx)
+                if self.in_grid(ny,nx):
+                    yield (ny,nx)
     
-    #checks if a cell is active
-    def is_active(self, y, x):
-        return self.grid[y][x] > 0
-
+    #returns all possible moves that can be made
     def moves(self):
         for y in range(self.rows):
             for x in range(self.cols):
@@ -64,17 +68,30 @@ class State():
                     new_grid[y][x] -= 1
                     yield State(new_grid)
 
+    #makes a move on the grid itself
     def makemove(self,y,x):
         self.grid[y][x]-=1
         return
-
+    
+    '''
+    This function finds regions by first creating a visited variable that will be a copy of the grid size but
+    with every value inside set to false. this then once it encounters an active value it will iterate through any 
+    adjacent active values until it stops finding any and that will be classified as a reigon and also will be classed as visited so
+    that the same reigon cant be counted more than once 
+    '''
     def numRegions(self):
-        visited = [[False] * self.cols for _ in range(self.rows)]
+        visited = []
+        for i in range(self.rows):
+            row = []
+            for j in range(self.cols):
+                row.append(False)
+            visited.append(row)
+
         regions = 0
 
         for i in range(self.rows):
             for j in range(self.cols):
-                if self.is_active(i, j) and not visited[i][j]:
+                if self.is_active(i, j) and visited[i][j]== False:
                     regions += 1
                     queue = deque([(i, j)])
                     visited[i][j] = True
@@ -89,31 +106,36 @@ class State():
 
     def numHingers(self):
         count = 0
-        base_regions = self.numRegions()
+        regions = self.numRegions()
 
         for i in range(self.rows):
             for j in range(self.cols):
                 if self.grid[i][j] == 1:
                     # Simulate removing this cell
-                    new_grid = deepcopy(self.grid)
-                    new_grid[i][j] = 0
-                    new_state = State(new_grid)
-                    new_regions = new_state.numRegions()
+                    ngrid = deepcopy(self.grid)
+                    ngrid[i][j] = 0
+                    nstate = State(ngrid)
+                    nregions = nstate.numRegions()
 
-                    if new_regions > base_regions:
+                    if nregions > regions:
                         count += 1
         return count
     
     def to_tuple(self):
-        """Return an immutable representation of the grid for hashing/comparison."""
-        return tuple(tuple(row) for row in self.grid)
+        result = []
+        for row in self.grid:
+            new_row = []
+            for val in row:
+                new_row.append(val)
+            result.append(tuple(new_row))
+        return tuple(result)
 
 
 def tester():
-    print("=== Hinger Game: State Tester ===")
+    print("State Class test")
 
     sa_grid = [
-        [0, 1, 0, 0, 2],
+        [0, 1, 0, 0, 1],
         [1, 1, 0, 0, 0],
         [0, 1, 1, 1, 0],
         [0, 0, 0, 0, 0],
@@ -121,15 +143,15 @@ def tester():
 
     sa = State(sa_grid)
     print("State A:\n", sa)
-    print("Number of regions:", sa.numRegions())
-    print("Number of hingers:", sa.numHingers())
+    print("Num of regions: ", sa.numRegions())
+    print("Num of hingers: ", sa.numHingers())
 
     print("\nPossible next states (1-move each):")
     for idx, next_state in enumerate(sa.moves(), 1):
-        print(f"\nMove {idx}:")
+        print("\nMove ",idx,": ")
         print(next_state)
 
-    print("\nTester completed.")
+    print("\nTest completed.")
 
 if __name__ == "__main__":
     tester()
